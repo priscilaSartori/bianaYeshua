8da21183e21d4da548ec8c351e310f40545b8190
Priscila Sartori <priscilasartori2@gmail.com>
2023-12-04 16:43:36 -0300

    feat: carrocel novidades

--- src/app/page/novidades/novidades.component.ts
+++ src/app/page/novidades/novidades.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from 'src/app/interfaces/product';
 import { ProductService } from '../../services/product.service';
 
@@ -9,9 +9,35 @@ import { ProductService } from '../../services/product.service';
 })
 export class NovidadesComponent {
   productsNovidades: Product[] = this.productService.products.filter((launch) => launch.isLaunch === true);
-  
+
   constructor(
     private productService: ProductService,
   ) {}
 
+  displayedProducts = this.productsNovidades.slice(0, 4);
+  currentIndex = 0;
+  translateX = 0;
+  itemsPerPage = 4; 
+
+  prevSlide() {
+    if (this.currentIndex > 0) {
+      this.currentIndex -= 1;
+      this.updateTranslateX();
+    }
+  }
+
+  nextSlide() {
+    const maxIndex = this.productsNovidades.length - this.itemsPerPage;
+    if (this.currentIndex < maxIndex) {
+      this.currentIndex += 1;
+      this.updateTranslateX();
+    }
+  }
+
+  updateTranslateX() {
+    this.displayedProducts = this.productsNovidades.slice(
+      this.currentIndex,
+      this.currentIndex + this.itemsPerPage
+    ); 
+  }
 }
